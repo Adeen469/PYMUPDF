c53bd3d1b53623b09ee52b0e8232663101934a0d
Ansari Adeen <mr.adeen469@gmail.com>
2026-03-02 03:03:28 +0530

    Add files via upload

---
+++ annot_postprocess.py
@@ -0,0 +1,23 @@
+"""
+Function: fitz.annot_postprocess
+Signature: annot_postprocess(page: 'Page', annot: 'Annot') -> None
+Description:
+Clean up after annotation insertion.
+
+Set ownership flag and store annotation in page annotation dictionary.
+"""
+
+import fitz
+
+# Source code of fitz.annot_postprocess:
+def annot_postprocess(page: "Page", annot: "Annot") -> None:
+    """Clean up after annotation insertion.
+
+    Set ownership flag and store annotation in page annotation dictionary.
+    """
+    #annot.parent = weakref.proxy(page)
+    assert isinstance( page, Page)
+    assert isinstance( annot, Annot)
+    annot.parent = page
+    page._annot_refs[id(annot)] = annot
+    annot.thisown = True
